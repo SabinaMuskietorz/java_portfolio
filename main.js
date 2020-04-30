983693891269824c939d7caf6f024d38aad69faf
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2020-04-30 11:12:04 +0100

    first commit

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-/* SCRIPTS.JS */
+/* MAIN.JS */
 
 function changePic() {
-    document.getElementById("profilePic").src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/594589/adrian-prp.jpg";
-  }
-  
-  function normalPic() {
-    document.getElementById("profilePic") .src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/594589/adrian.webp";
-  }
+  document.getElementById("profilePic").src = "images/portfolio21.jpg";
+}
+
+function normalPic() {
+  document.getElementById("profilePic").src = "images/pic2.jpg";
+}
+
+
+$(".go-to-contact").click(function() {
+  $('html, body').animate({
+      scrollTop: $(".contact").offset().top
+  }, 1000);
+});
